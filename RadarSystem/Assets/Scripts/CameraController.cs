79dc674c110780de6903a75b6ac0092984862f39
Eiichi Takebuchi <nanashi4129@gmail.com>
2021-05-24 14:35:34 +0900

    add project

---
+++ RadarSystem/Assets/Scripts/CameraController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    Vector2 prev;
+
+    Transform ts;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        prev = Input.mousePosition;
+        ts = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 cur = Input.mousePosition;
+
+        if (Application.isEditor)
+        {
+            Debug.Log("enter");
+            if (Input.GetMouseButtonDown(0))
+            {
+                var velocity = (cur - prev) * Time.deltaTime;
+                var displacement = new Vector3(velocity.x, 0f, velocity.y);
+                Debug.Log(velocity);
+                ts.position += displacement;
+            }
+        }
+        else
+        {
+
+        }
+
+        prev = cur;
+    }
+}
